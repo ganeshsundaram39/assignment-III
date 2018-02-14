71ff4886e24c256fbd5404d889ed4055c098974d
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2018-02-14 22:57:06 +0530

    responsive cover section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
-	$('input[name="fbtoken"]').focus(function(){
-		$('#search').css({'background-color':'#4080ff','color':'#fff'});
-	});
-	$('input[name="fbtoken"]').blur(function(){
-		$('#search').css({'background-color': '#fff','color':'#6c757d'});
-	});
+
+function userExperience() {
+        $('input[name="fbtoken"]').focus(function() {
+            $('#search').css({ 'background-color': '#4080ff', 'color': '#fff' });
+        });
+        $('input[name="fbtoken"]').blur(function() {
+            $('#search').css({ 'background-color': '#fff', 'color': '#6c757d' });
+        });
+
+        $('#timeline').click(function(){
+        	$('#about img').css({'display':'none'});
+        	$('#timeline img').css({'display':'block'});
+        	$('#about').css({'color':'#365899'});
+        	$('#timeline').css({'color':'#4b4f56'});
+        	
+        });
+
+        $('#about').click(function(){
+        	$('#timeline img').css({'display':'none'});
+        	$('#about img').css({'display':'block'});
+        	$('#about').css({'color':'#4b4f56'});
+        	$('#timeline').css({'color':'#365899'});
+        });
+    }
+
+$(document).ready(function() {
+
+	userExperience();
 
 });
-
